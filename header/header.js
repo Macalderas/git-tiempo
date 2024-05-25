ce0788c5131e58dbeeff68f89f196186971f563b
mynor <macalderas@scl.edu.gt>
2024-05-24 23:23:09 -0600

    Mensaje del commit

---
+++ header/header.js
@@ -0,0 +1,7 @@
+let header=document.querySelector(".header");
+header.innerHTML=`
+<header class="header">
+<h1 class="titulo">Comandos</h1>
+<img src="https://1000marcas.net/wp-content/uploads/2019/11/McDonalds-logo-768x432.png" alt="Mac" <header class="header-image">
+</header>
+    `;
