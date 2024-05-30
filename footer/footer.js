7492268f200342679fcc3c1eef307a0af6fa8fcb
mynor <macalderas@scl.edu.gt>
2024-05-29 20:07:12 -0600

    cambios

---
+++ footer/footer.js
@@ -0,0 +1,7 @@
+let footer=document.querySelector(".footer");
+footer.innerHTML=`
+<footer>
+<h2> scl.edu.gt</h2>
+<img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Coca_Cola_red_and_white.png" alt="Imagen del Footer">
+</footer>
+`;
